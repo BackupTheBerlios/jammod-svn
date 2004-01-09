0583908e006e6c9d6fa9a9d3856f7d619ba1004b
max <max@12331426-51cf-0310-9b87-87b2fd312d51>
2004-01-09 23:19:16 +0000

    reworked makefiles, README additions

--- trunk/config.mk
+++ trunk/config.mk
@@ -1,12 +1,10 @@
+#
+# build configuration... edit this file
+#
+
+PREFIX = /usr/local
+
 CC = gcc
 CFLAGS = -g -O0
 
-KERNEL_HEADERS = /usr/src/linux-2.6.0/include
-
-# do not change stuff below.
-
-PACKAGE_NAME = jammod
-PACKAGE_VERSION = 1.0pre1
-
-CFLAGS += -DPACKAGE_NAME=\"$(PACKAGE_NAME)\" -DPACKAGE_VERSION=\"$(PACKAGE_VERSION)\"
-
+KERNEL_HEADERS = /usr/src/linux/include
--- trunk/src/test.c
+++ trunk/src/test.c
@@ -1,11 +1,8 @@
 #include <linux/kernel.h>
 
-int the_wall = 0xdeadbeef;
-
 int init_module(void);
 int init_module(void) {
-    printk("is there anybody out there?\n");
-    printk("just nod if you can hear me\n");
-    the_wall = 1;
-    return -2;
+    printk(KERN_EMERG "is there anybody out there?\n");
+    printk(KERN_EMERG "just nod if you can hear me\n");
+    return -1;
 }
